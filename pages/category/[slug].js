fe0426ceec7f187b782c6079a9df1fd5353d0085
Bryan Robinson <bryanlrobinson@gmail.com>
2022-10-28 10:17:56 -0400

    Removes title from category product grid

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -30,10 +30,7 @@ export default function Page({ category }) {
             title={category.name}
             description={category.description}
         />
-        <section className='mt-20'>
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900">{category.name} Products</h2>
 
-            <ProductGrid products={category.products} />
-        </section>
+        <ProductGrid  products={category.products} />
     </>)
 }
